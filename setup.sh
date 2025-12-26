c978a440b6f42b3b15bc5f0007d8ff8edf91c828
Tim Sawicki <truthordata@gmail.com>
2025-12-26 16:28:39 -0500

    fix main setup script

--- setup.sh
+++ setup.sh
@@ -3,10 +3,14 @@ set -euo pipefail
 
 APP_CONFIG_FOLDER="/mnt/user/appconfig"
 
-read -r -p "Enter desired destination directory (blank defaults to: ${APP_CONFIG_FOLDER}): " DEST
+read -r -p "Enter your desired config folder (blank defaults to: ${APP_CONFIG_FOLDER}): " DEST
 DEST="${DEST:-$APP_CONFIG_FOLDER}"
 
-mkdir -p "$DEST"
+if [[ ! -d "$DEST" ]]; then
+  echo "Error: destination folder does not exist: $DEST. \nIdeally it should be an unraid share \
+  directory with its storage set to your primary drive." >&2
+  exit 1
+fi
 
 # skips existing dirs
 for dir in */; do
@@ -18,8 +22,5 @@ for dir in */; do
   cp -a "$dir" "$DEST/"
 done
 
-echo "Done."
-
-
-
-echo "Copied app config folders to ${DEST}; use this location now for app configuration!"
+echo "Initial setup complete: copied app config folders to ${DEST}. \
+\nUse this location now for app configuration!"
